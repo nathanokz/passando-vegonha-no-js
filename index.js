00246cd5aaf02b5133aa66bbbe0b5859dd9461fe
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-05-27 12:10:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 console.log("script iniciado...");
 
 const inputNome = document.getElementById("inputNome");
-const inputIdade = document.getElementById("inputIdade");
+const inputCPF = document.getElementById("inputCPF");
 const btnSalvar = document.getElementById("btnSalvar");
-const divResponse = document.getElementById("responsiveContainer")
+const inputIdade = document.getElementById("inputIdade");
+const divResponse = document.getElementById("responsiveContainer");
 
 btnSalvar.addEventListener("click", btnAction);
 
-function btnAction(){
-    console.log("teste");
+function btnAction() {
+    console.log("TESTE!");
 
     let nome = inputNome.value;
+    let cpf = inputCPF.value;
     let idade = inputIdade.value;
 
     console.log(nome);
+    console.log(cpf);
     console.log(idade);
 
     inputNome.value = "";
     inputIdade.value = "";
+    inputCPF.value = "";
 
-    divResponse.innerHTML += `<strong> ${nome} </strong>`;
-    divResponse.innerHTML += `<strong> ${idade} </strong`;
+    divResponse.innerHTML += `<strong>Seu nome é:  ${ nome }</strong><br></br>`;
+    divResponse.innerHTML += `<strong>Você tem:  ${ idade } anos</strong><br></br>`;
+    divResponse.innerHTML += `<strong>Seu cpf é:  ${ cpf } SEU CPF JA ERA!</strong><br></br>`;
 }
